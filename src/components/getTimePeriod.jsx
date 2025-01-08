4d551990d4ff9243d806dc6c511ec71233c71211
Toluwalashe <asheogunleye02@gmail.com>
2025-01-09 00:02:19 +0100

    update TimePeriod function

--- src/components/getTimePeriod.jsx
+++ src/components/getTimePeriod.jsx
@@ -1,7 +1,7 @@
 const getTimePeriod = () => {
     const now = new Date();
     const hour = now.getHours();
-    const greeting = hour < 12 ? 'morning' : hour < 18 ? 'afternoon' : 'evening'
+    const greeting = hour >= 4 && hour < 12  ? 'morning' : hour >= 12 && hour < 18 ? 'afternoon' : 'evening'
 
     return greeting
 }
